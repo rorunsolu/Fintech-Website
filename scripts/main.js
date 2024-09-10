1b283dcd5ae76eb708d2fca362182b2932eb9bae
Ray Orunsolu <rorunsolu@gmail.com>
2024-09-10 15:19:26 +0100

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+import viteLogo from '/vite.svg';
+
+import '/styles/style.scss';
+
+import '/styles/navbar.scss';
+import '/styles/header.scss';
+import '/styles/footer.scss';
+
+import '/styles/typograhpy.scss';
+import '/styles/button.scss';
+
+import '/styles/trust.scss';
+import '/styles/partners.scss';
+import '/styles/empower.scss';
+import '/styles/tracking.scss';
+import '/styles/offers.scss';
+import '/styles/growth.scss';
+import '/styles/testimonials.scss';
