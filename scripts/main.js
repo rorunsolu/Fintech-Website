14657d41d1e1093fa90aa031686fadd6e441e564
Ray Orunsolu <rorunsolu@gmail.com>
2024-10-27 20:51:59 +0000

    ignore

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ import '/styles/style.scss';
 import '/styles/typography.scss';
 import '/styles/utilities.scss';
 import '/styles/navbar.scss';
+import '/styles/menu.scss';
 import '/styles/header.scss';
 import '/styles/trust.scss';
 import '/styles/partners.scss';
