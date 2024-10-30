d87fe31703a5ed553dbfb97909709877478ddf33
Ray Orunsolu <rorunsolu@gmail.com>
2024-10-30 18:17:11 +0000

    changed location of main js

---
+++ main.js
@@ -0,0 +1,14 @@
+import '/styles/style.scss';
+import '/styles/typography.scss';
+import '/styles/utilities.scss';
+import '/styles/navbar.scss';
+import '/styles/menu.scss';
+import '/styles/header.scss';
+import '/styles/trust.scss';
+import '/styles/benefits.scss';
+import '/styles/tracking.scss';
+import '/styles/pricing.scss';
+import '/styles/conversion.scss';
+import '/styles/testimonials.scss';
+import '/styles/footer.scss';
+
