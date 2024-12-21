81b6808da135f2d6293573364a39e22561aeac3d
Ray Orunsolu <rorunsolu@gmail.com>
2024-12-21 14:41:07 +0000

    minor changes to styles + changed folder structure

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import '/styles/style.scss';
-import '/styles/typography.scss';
-import '/styles/utilities.scss';
-import '/styles/navbar.scss';
-import '/styles/menu.scss';
-import '/styles/header.scss';
-import '/styles/trust.scss';
-import '/styles/benefits.scss';
-import '/styles/tracking.scss';
-import '/styles/pricing.scss';
-import '/styles/conversion.scss';
-import '/styles/testimonials.scss';
-import '/styles/footer.scss';
+import './styles/style.scss';
+import './styles/typography.scss';
+import './styles/utilities.scss';
+import './styles/navbar.scss';
+import './styles/menu.scss';
+import './styles/header.scss';
+import './styles/trust.scss';
+import './styles/benefits.scss';
+import './styles/tracking.scss';
+import './styles/pricing.scss';
+import './styles/conversion.scss';
+import './styles/testimonials.scss';
+import './styles/footer.scss';
 
 let openMenuBarBtn = document.getElementById('js-open-menu-btn');
 
